bb018fc7ed77fa1e015b0a7918c25860b2395382
Caio César <caiogalaxy00@gmail.com>
2025-01-23 11:23:21 -0300

    Service layer, component registration

--- src/main/java/com/caiocesar/course/resources/UserResource.java
+++ src/main/java/com/caiocesar/course/resources/UserResource.java
@@ -1,18 +1,34 @@
 package com.caiocesar.course.resources;
 
 import com.caiocesar.course.entities.User;
+import com.caiocesar.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    public UserService service;
+
     @GetMapping
-    public ResponseEntity<User> FindAll() {
-        User u = new User(1L, "Caio", "caio@gmail.com", "55555", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> FindAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
 }
